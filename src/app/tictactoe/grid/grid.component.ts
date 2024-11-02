72c39912ea69dda7188b0be5bcab2ca3a8115d07
Mihai Borza <m.borza@HTBLALeonding.onmicrosoft.com>
2024-11-02 19:14:53 +0100

    minor improvements

--- src/app/tictactoe/grid/grid.component.ts
+++ src/app/tictactoe/grid/grid.component.ts
@@ -9,11 +9,11 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent {
-  @Input() grid: string[][] = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
+  @Input() grid: string[][] = this.initializeGrid();
+
+  private initializeGrid(): string[][] {
+    return Array(3).fill(null).map(() => Array(3).fill(''));
+  }
   @Output() cellClicked = new EventEmitter<{ row: number, col: number }>();
 
   onCellClicked(row: number, col: number) {
